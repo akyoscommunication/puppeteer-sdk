e580d48489c2e469e12cc157bb1e79cf94e8f84c
Lilian Carneau <lilian.akyos@gmail.com>
2023-06-15 14:23:41 +0200

    feat: name in opened page

--- Service/Puppeteer.php
+++ Service/Puppeteer.php
@@ -4,11 +4,10 @@ namespace Akyos\PuppeteerSDK\Service;
 
 use Firebase\JWT\JWT;
 use Symfony\Component\DependencyInjection\ContainerInterface;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\Filesystem\Filesystem;
-use Symfony\Component\HttpClient\Response\TraceableResponse;
 use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
@@ -56,15 +55,28 @@ class Puppeteer
     {
         $fetch = $this->fetch($url, $paramsUrl, $params);
         $response = $this->getResponse($fetch);
-        $response->headers->set('Content-Disposition', 'attachment;filename=' . $filename);
+        $response->headers->set('Content-Disposition', $response->headers->makeDisposition(ResponseHeaderBag::DISPOSITION_ATTACHMENT, $filename));
+        $response->headers->set('Content-Description', $filename);
 
         return $response;
     }
 
-    public function open($filename, $url, $paramsUrl = [], $params = []): Response
+    public function open($filename, $url, $paramsUrl = [], $params = [], $customName = null): Response
     {
         $fetch = $this->fetch($url, $paramsUrl, $params);
-        return $this->getResponse($fetch);
+        $response = $this->getResponse($fetch);
+        $response->headers->set('Content-Disposition', $response->headers->makeDisposition(ResponseHeaderBag::DISPOSITION_INLINE, $filename));
+        $response->headers->set('Content-Description', $filename);
+
+        /**
+         * Permet d'insérer le titre de la page dans le binary.
+         */
+        $title = str_replace(['(', ')'], '', $customName ?: $filename);
+        $title = utf8_decode($title);
+        $updatedContent = preg_replace('/<<\/Creator/', '<</Title (' . $title . ') /Creator', $response->getContent());
+
+        $response->setContent($updatedContent);
+        return $response;
     }
 
     public function save($output, $url, $paramsUrl = [], $params = []): File
